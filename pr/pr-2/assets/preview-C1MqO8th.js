5a936db6cc7334239a84c3bffd5c6238ccd0fc68
megheaiulian <megheaiulian@users.noreply.github.com>
2025-09-24 05:26:42 +0000

    Remove preview for PR 2 🛬

--- pr/pr-2/assets/preview-C1MqO8th.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./DocsRenderer-NNNQARDV-BlqWzE0N.js","./iframe-Bz_Sp4H_.js","./doctrine-DN03mvdw.js","./index-Cu4lwwaE.js","./index-DrFu-skq.js"])))=>i.map(i=>d[i]);
-import{_ as t}from"./iframe-Bz_Sp4H_.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-BlqWzE0N.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4]),import.meta.url);return new r}}};export{_ as parameters};
